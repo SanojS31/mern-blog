d7f4fcc00e1b68fa406e34385bcdfb7eeae483d4
SanojS31 <sanojtindu@gmail.com>
2025-03-13 15:11:11 +0530

    add OAuth component to Signin and Signup pages

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,8 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+export default function OAuth() {
+  return (
+    <Button type='button' gradientDuoTone='purpleToBlue' outline>OAuth</Button>
+  )
+}
